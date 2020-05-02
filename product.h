9cd3ff09c72e94b2b734690385ac9b0fd6e3e37c
HwangSoobin <21900813@handong.edu>
2020-05-02 23:42:54 +0900

    Update product.h

--- product.h
+++ product.h
@@ -11,7 +11,7 @@ typedef struct{
 	int event; //이벤트중인 화장품  
 } Product;
 
-int addProduct(Product *p); //제품을 추가하는 함수
+int createProduct(Product *p); //제품을 추가하는 함수
 void readProduct(Product *p); //하나의 제품을 출력하는 함수
 int deleteProduct(Product *p); //제품을 삭제하는 함수 
 int updateProduct(Product *p); //제품을 수정하는 함수
