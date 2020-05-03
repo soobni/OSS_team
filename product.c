ca908f6139c6145a91025ffc30a0f3ca9a188496
HwangSoobin <21900813@handong.edu>
2020-05-03 17:56:45 +0900

    Update createProduct()

--- product.c
+++ product.c
@@ -15,8 +15,10 @@ int createProduct(Product *p){
 	scanf("%d",p->weight);
 	printf("제품의 별점은? ");
 	scanf("%d", p->star);
+	printf("제품의 personal color는? :(1: 봄웜 2: 여름쿨 3: 가을웜 4: 겨울쿨) ");
+	scanf("%d",p->pcolor);
 	printf("제품의 색상은? ");
-	// 색상 구현 생각..
+	// 색은 string으로 받을까? 아님 숫자로 받을까? 숫자로 받으면 색의 종류는 어떤식으로 나눌까?	
 	printf("이벤트 유무? Y:1 N:2 ");
 	scanf("%d",p->event);
 
