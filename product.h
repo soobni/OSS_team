4235ec5ead68d14898439666c3d74a02ae942092
Park-Minjoo <61863242+Park-Minjoo@users.noreply.github.com>
2020-05-04 16:21:41 +0900

    edit readProduct(Product p);

--- product.h
+++ product.h
@@ -13,6 +13,6 @@ typedef struct{
 } Product;
 
 int createProduct(Product *p); //제품을 추가하는 함수
-void readProduct(Product *p); //하나의 제품을 출력하는 함수
+void readProduct(Product p); //하나의 제품을 출력하는 함수
 int deleteProduct(Product *p); //제품을 삭제하는 함수 
 int updateProduct(Product *p); //제품을 수정하는 함수
