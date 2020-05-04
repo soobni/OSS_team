4e7dc70470edf4d545f426e1ea135008ddd9deec
HwangSoobin <21900813@handong.edu>
2020-05-04 18:59:53 +0900

    Update evenctfunction

--- manager.h
+++ manager.h
@@ -9,5 +9,5 @@ void PersonalColor(Product *p); //자신의 퍼스널 컬러에 맞는 화장품
 int selectDataNo(Product *p, int count); //제품의 번호를 선택하는 함수
 int loadData(Product *p); // 파일을 불러오는 함수
 void rankingProduct(Product *p); //제품을 별점순대로 보여주는 함수
-void eventPoduct(Product *p); //이벤트하는 제품들을 보여주는 함수
+void eventPoduct(Product *p,int count); //이벤트하는 제품들을 보여주는 함수
 int selectColor(); // 제품의 색을 선택하는 함수
