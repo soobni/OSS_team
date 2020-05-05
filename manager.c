60c604f86cce9c1b98b18f25536989c492e9c0cf
Park Minjoo <21800268@handong.edu>
2020-05-05 16:37:36 +0900

    add selectMenu()

--- manager.c
+++ manager.c
@@ -1,5 +1,19 @@
 #include "manager.h"
 
+int selectMenu(){
+	int menu;
+	printf("\n***What do you want? ***\n"); 
+	printf("1. 조회\n"); 
+	printf("2. 추가\n"); 
+	printf("3. 수정\n"); 
+	printf("4. 삭제\n"); 
+	printf("5. 파일저장\n"); 
+	printf("6. 이름검색\n"); 
+	printf("0. 종료\n\n"); 
+	printf("=> 원하는 메뉴는? "); 
+	scanf("%d", &menu); 
+	return menu;
+}
 
 int selectDataNo(Product *p, int count){
     int no=0;
@@ -20,3 +34,5 @@ void eventProduct(Product *p,int count){
 		else continue;
 	}
 }
+
+
