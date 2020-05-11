019ecf7d725db9cc80f51f4d0af99bc7b8e46b5e
Park Minjoo <21800268@handong.edu>
2020-05-11 23:40:39 +0900

    이벤트제품조회

--- manager.c
+++ manager.c
@@ -9,6 +9,7 @@ int selectMenu(){
 	printf("4. 삭제\n"); 
 	printf("5. 파일저장\n"); 
 	printf("6. 이름검색\n"); 
+	printf("7. 이벤트 제품 조회 \n");
 	printf("0. 종료\n\n"); 
 	printf("=> 원하는 메뉴는? "); 
 	scanf("%d", &menu); 
